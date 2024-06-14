872336b87b4c95dbe91725df9e86630c18165cb8
Ethiraj Mudhaliar <ethirajmudhaliar@Ethirajs-MBP.lan>
2024-06-13 22:24:47 -0500

    Initial commit with React frontend setup

---
+++ frontend/src/components/About.js
@@ -0,0 +1,14 @@
+// src/components/About.js
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is the about section of my resume.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import About from './components/About';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <div>
+      <About />
+      <Experience />
+      <Education />
+      <Projects />
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
